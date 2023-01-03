05c6c0bbf73e706ebc3b9a163eee602c68302863
Christian Bachhelm <cbachhelm@tuta.io>
2023-01-03 12:38:02 +0100

    Use vuetify in the app

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,28 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
 import './assets/main.css'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
