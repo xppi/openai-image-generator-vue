c51956fa26ede9426589241237539fd5744892bd
Christian Bachhelm <cbachhelm@tuta.io>
2023-01-05 17:35:09 +0100

    Add a bit of fluff text in the about section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h4 class="text-h4">DALL·E 2</h4>
+    <p>
+      This is a small proof of concept
+      <a href="https://vuejs.org/" target="_blank">VueJs</a> app to generate 
+      <a href="https://openai.com/dall-e-2/" target="_blank">OpenAi DALL·E</a> 
+      images.
+    </p>
+    <p>DALL·E 2 is a new AI system that can create realistic images and art from a description in natural language.</p>
+    <p>It can create original, realistic images and art from a text description. It can combine concepts, attributes, and styles.</p>
   </div>
 </template>
 
-<style>
+<style scoped>
+h3 {
+  margin-bottom: 24px;
+}
+
+p {
+  font-size: 18px;
+  margin: 24px 0 24px 0;
+}
 </style>
