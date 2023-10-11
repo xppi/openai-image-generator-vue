7a29318e77f84776867daef93c65d86eae0b8583
xppi <59060461+xppi@users.noreply.github.com>
2023-10-11 23:17:11 +0200

    dawd

--- src/lib/openaiApi.ts
+++ src/lib/openaiApi.ts
@@ -4,6 +4,10 @@ const configuration: Configuration = new Configuration({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY
 })
 
+configuration.baseOptions.headers = {
+  Authorization: "Bearer " + "YOURE_OPENAI_KEY",
+};
+
 const openaiApi: OpenAIApi = new OpenAIApi(configuration)
 
 export { openaiApi }
