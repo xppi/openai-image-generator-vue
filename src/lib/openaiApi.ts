1f34d900c187f89a26616e55391dc2d681c896d9
xppi <59060461+xppi@users.noreply.github.com>
2023-10-12 15:51:49 +0200

    Update openaiApi.ts

--- src/lib/openaiApi.ts
+++ src/lib/openaiApi.ts
@@ -4,9 +4,6 @@ const configuration: Configuration = new Configuration({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY
 })
 
-configuration.baseOptions.headers = {
-  Authorization: "Bearer " + "YOURE_OPENAI_KEY",
-};
 
 const openaiApi: OpenAIApi = new OpenAIApi(configuration)
 
